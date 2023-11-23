94037183400ce7f6313228b677beeadb1130a107
Dmitriy Zuborev <Tyuiu.ZuborevDA@outlook.com>
2023-11-23 20:59:41 +0500

    Task4

---
+++ Tyuiu.ZuborevDA.Sprint5.Task4.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZuborevDA.Sprint5.Task4.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZuborevDA.Sprint5.Task4.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eada7d00-2e12-45fb-bf5e-a2ab4f03d4ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZuborevDA.Sprint5.Task4.V27/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.ZuborevDA.Sprint5.Task4.V27.Lib;
+
+namespace Tyuiu.ZuborevDA.Sprint5.Task4.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Зуборев Д. А. | АСОиУБ-23-2";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                  *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                    *");
+            Console.WriteLine("* Задание #4                                                                 *");
+            Console.WriteLine("* Вариант #27                                                                *");
+            Console.WriteLine("* Выполнил: Зуборев Дмитрий Александрович | АСОиУБ-23-2                      *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Дан файл С:/DataSprint5/InPutDataFileTask4V27.txt в котором есть           *");
+            Console.WriteLine("* вещественное значение. Прочитать значение из файла и подставить вместо Х   *");
+            Console.WriteLine("* в формуле y = ((x^3)-4x)/cosx. Вычислить значение по формуле (Полученное   *");
+            Console.WriteLine("* значение округлить до трёх знаков после запятой) и вернуть полученный      *");
+            Console.WriteLine("* результат на консоль.                                                      *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            string path = $@"C:\DataSprint5\InPutDataFileTask4V27.txt";
+
+            Console.WriteLine($"Данные находятся в файле: {path}");
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
